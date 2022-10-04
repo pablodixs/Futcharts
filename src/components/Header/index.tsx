14f80cb93ff60fa0bb2291fb4beefbb835872fbc
pablodixs <pabllosoarez@gmail.com>
2022-10-04 17:30:18 -0300

    add dropdown menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,99 @@
-import { HeaderContainer, Logo } from "./styles";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+
+import {
+  CampHighlight,
+  CampLinkContainer,
+  CampLinkListContainer,
+  ClubLinkContainer,
+  HeaderContainer,
+  ImgHighlight,
+  LinkCamps,
+  LinkClubs,
+  Logo,
+  NavContent,
+} from "./styles";
+
+import cupLogo from "../../assets/cuplogo.png";
+import { SquaresFour } from "phosphor-react";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <nav>
-        <ul>
-          <li>Página Inicial</li>
-          <li>Campeonatos</li>
-          <li>Clubes</li>
-        </ul>
-      </nav>
+      <NavigationMenu.Root>
+        <NavigationMenu.List>
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger>Página Inicial</NavigationMenu.Trigger>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger>Campeonatos</NavigationMenu.Trigger>
+            <NavContent>
+              <CampLinkContainer>
+                <CampHighlight>
+                  <ImgHighlight src={cupLogo} />
+                </CampHighlight>
+                <CampLinkListContainer>
+                  <LinkCamps href="#">
+                    Brasileirão Série A
+                    <ImgHighlight src="https://upload.wikimedia.org/wikipedia/pt/4/42/Campeonato_Brasileiro_S%C3%A9rie_A_logo.png" />
+                  </LinkCamps>
+                  <LinkCamps href="#">
+                    Champion's League
+                    <img src="https://pnggrid.com/wp-content/uploads/2021/04/UEFA-Champions-League-Logo-PNG-White-1024x939.png" />
+                  </LinkCamps>
+                  <LinkCamps href="#">
+                    Premier League
+                    <img src="https://imagecache.365scores.com/image/upload/f_png,w_250,h_250,c_limit,q_auto:eco,d_Countries:Round:1.png/v2/Competitions/7" />
+                  </LinkCamps>
+                  <LinkCamps href="#">
+                    Ver todos
+                    <SquaresFour size={32} color="#f7f7f7" weight="duotone" />
+                  </LinkCamps>
+                </CampLinkListContainer>
+              </CampLinkContainer>
+            </NavContent>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger>Clubes</NavigationMenu.Trigger>
+            <NavContent>
+              <span>Clubes em destaque</span>
+              <ClubLinkContainer>
+                <LinkClubs href="#">
+                  <img
+                    src="https://apifutebol.s3.sa-east-1.amazonaws.com/escudos/5f999c95084cb.svg"
+                    alt="Escudo Flamengo"
+                  />
+                  Flamengo
+                </LinkClubs>
+                <LinkClubs href="#">
+                  <img
+                    src="https://apifutebol.s3.sa-east-1.amazonaws.com/escudos/5f999ccdc1656.svg"
+                    alt="Escudo São Paulo"
+                  />
+                  São Paulo
+                </LinkClubs>
+                <LinkClubs href="#">
+                  <img
+                    src="https://apifutebol.s3.sa-east-1.amazonaws.com/escudos/5f999d7ab48fa.svg"
+                    alt="Escudo Corinthians"
+                  />
+                  Corinthians
+                </LinkClubs>
+                <LinkClubs href="#">
+                  <img
+                    src="https://upload.wikimedia.org/wikinews/en/a/a7/Paris_Saint-Germain_F.C..svg"
+                    alt="Escudo PSG"
+                  />
+                  PSG
+                </LinkClubs>
+                <LinkClubs href="#">
+                  <SquaresFour size={32} color="#f7f7f7" weight="duotone" />
+                  Ver todos
+                </LinkClubs>
+              </ClubLinkContainer>
+            </NavContent>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
       <Logo />
     </HeaderContainer>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,21 +1,32 @@
-import styled from "styled-components";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import styled, { keyframes } from "styled-components";
 
-import logoImage from '../../assets/logo.svg'
+import logoImage from "../../assets/logo.svg";
+
+const scaleIn = keyframes`
+  from { 
+    opacity: 0;
+    transform: translateY(-20px); }
+  to { 
+    opacity: 1; 
+    transform: translateY(0px);
+  }
+`;
 
 export const HeaderContainer = styled.header`
-  background-color: ${props => props.theme.black900};
+  background-color: ${(props) => props.theme.black900};
   padding: 1.5rem;
   border-bottom: 1px solid #171717;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   position: relative;
   max-width: 1280px;
   margin: 0 auto;
 
   *:hover {
-      transition: all ease 150ms
-    }
+    transition: all ease 150ms;
+  }
 
   nav {
     position: absolute;
@@ -23,22 +34,136 @@ export const HeaderContainer = styled.header`
 
     ul {
       list-style: none;
-      
+
       li {
         display: inline-block;
-        padding: .5rem 1rem;
-        border-radius: 3px;
-        font-weight: 550;
-        font-size: 0.875rem;
-        text-transform: uppercase;
 
-        &:hover {
-          background-color: ${props => props.theme.blueBrasileiraoA};
+        button {
+          padding: 0.5rem 1rem;
+          border-radius: 3px;
+          border: none;
+          font-weight: 550;
+          font-size: 0.875rem;
+          text-transform: uppercase;
+          background-color: transparent;
+          cursor: pointer;
+
+          &:hover {
+            background-color: ${(props) => props.theme.blueBrasileiraoA};
+          }
         }
       }
     }
   }
-`
+`;
+
+export const ClubLinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const CampLinkContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const CampLinkListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const CampHighlight = styled.div`
+  width: 12.5rem;
+  display: block;
+  background: linear-gradient(
+    310deg,
+    rgba(98, 6, 27, 1) 0%,
+    rgba(207, 15, 56, 1) 100%
+  );
+  border-radius: 3px;
+  padding: 1rem;
+`;
+
+export const NavContent = styled(NavigationMenu.Content)`
+  display: flex;
+  flex-direction: column;
+  margin-top: 0.5rem;
+  padding: 1rem;
+  border-radius: 5px;
+  border: 1px solid #212121;
+  background-color: ${(props) => props.theme.black900};
+  position: absolute;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
+    hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  animation: ${scaleIn} 150ms ease-in;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 1.125rem;
+    font-family: "FreightSansProBlack", sans-serif;
+    text-transform: uppercase;
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const LinkClubs = styled(NavigationMenu.Link)`
+  width: 6.25rem;
+  height: 6.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 1rem;
+  border-radius: 3px;
+  cursor: pointer;
+  text-decoration: none;
+  text-align: center;
+  font-size: 0.875rem;
+  font-weight: 500;
+  color: ${(props) => props.theme.grey300};
+
+  img {
+    width: auto;
+    height: 2.25rem;
+    object-fit: cover;
+  }
+
+  &:hover {
+    background-color: #202020;
+  }
+`;
+
+export const LinkCamps = styled(NavigationMenu.Link)`
+  width: 15rem;
+  height: 3.125rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  border-radius: 3px;
+  cursor: pointer;
+  text-decoration: none;
+  font-weight: 500;
+
+  img {
+    width: auto;
+    height: 2.25rem;
+    object-fit: cover;
+  }
+
+  &:hover {
+    background-color: #202020;
+  }
+`;
+
+export const ImgHighlight = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: contain;
+`;
 
 export const Logo = styled.div`
   background-image: url(${logoImage});
@@ -51,4 +176,4 @@ export const Logo = styled.div`
   &:hover {
     opacity: 0.5;
   }
-`
+`;
